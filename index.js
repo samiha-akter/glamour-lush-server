9c79ba6bb0144dd1c6e141bb7acb27954a4fd7ea
Samiha Akter <64433400+samiha-akter@users.noreply.github.com>
2024-11-20 23:59:13 +0600

    Contact API added

--- index.js
+++ index.js
@@ -14,8 +14,39 @@ app.use(
     origin: [`${process.env.BASE_URL}`],
     optionsSuccessStatus: 200,
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow Authorization header
+    methods: ["GET", "POST", "PATCH", "DELETE"],
   })
 );
+
+// token verification
+const verifyJWT = (req, res, next) => {
+  // console.log("Authorization Header:", req.headers);
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.send({ message: "No Token" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.send({ message: "Invalid Token" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// seller verification
+const verifySeller = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  if (user?.role !== "seller") {
+    return res.send({ message: "Forbidden Access" });
+  }
+  next();
+};
 
 // mongoDB
 const client = new MongoClient(url, {
@@ -27,15 +58,140 @@ const client = new MongoClient(url, {
 });
 
 const db = client.db("glamour-lush");
+const contactCollection = db.collection("contact");
+const userCollection = db.collection("users");
+const productCollection = db.collection("products");
 
 async function run() {
   try {
+
+    // Insert Contact Message
+    app.post("/contact", async (req, res) => {
+      const messageData = req.body;
+      const result = await contactCollection.insertOne(messageData);
+      res.send(result);
+    });
+
+    // Insert Users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Getting User Info
+    app.get("/user/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+
+    // Add Products
+    app.post("/add-products", verifyJWT, verifySeller, async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+
+    // Getting All Products
+    app.get("/all-products", async (req, res) => {
+      const { title, brand, sort, category, page = 1, limit = 6 } = req.query;
+      const query = {};
+      if (title) {
+        query.title = { $regex: title, $options: "i" };
+      }
+      if (category) {
+        query.category = { $regex: category, $options: "i" };
+      }
+      if (brand) {
+        query.brand = brand;
+      }
+
+      const pageNumber = Number(page);
+      const limitNumber = Number(limit);
+      const sortOption = sort === "asc" ? 1 : -1;
+
+      const products = await productCollection
+        .find(query)
+        .skip((pageNumber - 1) * limitNumber)
+        .limit(limitNumber)
+        .sort({ price: sortOption })
+        .toArray();
+
+      // const productInfo = await productCollection
+      //   .find({}, { projection: { category: 1, brand: 1 } })
+      //   .toArray();
+
+      const totalProducts = await productCollection.countDocuments(query);
+      const brands = [...new Set(products.map((product) => product.brand))];
+      const categories = [
+        ...new Set(products.map((product) => product.category)),
+      ];
+
+      res.json({ products, brands, categories, totalProducts });
+    });
+
+    // Wishlist Add
+    app.patch("/wishlist/add", verifyJWT, async (req, res) => {
+      const { userEmail, productId } = req.body;
+      const result = await userCollection.updateOne(
+        { email: userEmail },
+        {
+          $addToSet: { wishlist: new ObjectId(String(productId)) },
+        }
+      );
+      res.send(result);
+    });
+
+    // Get Wishlist Data
+    app.get("/wishlist/:userId", verifyJWT, async (req, res) => {
+      const { userId } = req.params;
+      const user = await userCollection.findOne({
+        _id: new ObjectId(String(userId)),
+      });
+      if (!user) {
+        return res.send({ message: "User Not Found" });
+      }
+      const wishlist = await productCollection
+        .find({
+          _id: { $in: user.wishlist || [] },
+        })
+        .toArray();
+
+      res.send(wishlist);
+    });
+
+    // Wishlist Remove
+    app.patch("/wishlist/remove", verifyJWT, async (req, res) => {
+      const { userEmail, productId } = req.body;
+      const result = await userCollection.updateOne(
+        { email: userEmail },
+        {
+          $pull : { wishlist: new ObjectId(String(productId)) },
+        }
+      );
+      res.send(result);
+    });
+
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
 }
 run().catch(console.dir);
 
+// JWT
+app.post("/authentication", async (req, res) => {
+  const userEmail = req.body;
+  const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, {
+    expiresIn: process.env.TOKEN_EXPIRATION,
+  });
+  res.send({ token });
+});
 app.get("/", (req, res) => {
   res.send("Glamour Lush is Running");
 });
